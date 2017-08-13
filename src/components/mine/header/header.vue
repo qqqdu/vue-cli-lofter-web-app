e193c96f3d9dafd33fa67ccd98412588c37f8261
nigulasizhaosi <1036971959@qq.com>
2017-08-14 01:02:36 +0800

    我的 页面header完成

--- src/components/mine/header/header.vue
+++ src/components/mine/header/header.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="header">
+
       <img :src="myImg">
+      <div class="myInf">
+          <div class="imgHead">
+                <img :src="headImg">
+          </div>
+          <div class="infRight">
+              <p>BlackHead<span>ID：BlackHead887</span></p>
+              <div>  
+                  <a>关注 <span>3</span></a>
+                  <a>喜欢 <span>2</span></a>
+                  <a>粉丝 <span>0</span></a>
+              </div>
+          </div>
+          <div class="h_arrowR"></div>
+      </div>
   </div>
 </template>
 
@@ -15,20 +30,8 @@ export default {
     return {
       __root:'../../assets',
       chooseIndex : 0,
+      headImg: require('../../../assets/user/head.png'),
       myImg: require('../../../assets/user/myImg.jpg'),
-      barType : [
-          '推荐',
-          '旅行',
-          '摄影',
-          '摄影课',
-          '音乐',
-          '设计',
-          '女神',
-          '运动',
-          '电影',
-          '娱乐',
-          '穿搭'     
-      ]
     }
   },
   computed : {
@@ -45,9 +48,28 @@ export default {
 <style scoped lang='less'>
 @imgSrc:'./assets';
 @centerColor : #a2a6a6;
-@height : 3rem;
 @width: 20rem;
-@lnHeight : 48px;
+@list5 : 5rem;
+@list0 : 4rem;
+@list1 : 3rem;
+@list2 : 2rem;
+@imgHead : 2.5rem;
+@1Font:1rem;
+@lMainFont:.9rem;
+@mainFont:.8rem;
+@smallFont:.6rem;
+@infA:#7d7d7d;
+@lessColor:#999999;
+@lesstColor:#d8d8d8;
+@mainColor:rgb(51, 51, 51);
+@green:rgb(154, 198, 0);
+@bule:rgb(91, 159, 201);
+@wordPadding:0 .5rem 0;
+@paddingTb:.5rem;
+@height3:3rem;
+@split:1rem;
+@headHeight:@height*3-1;
+@height : 3rem;
 .blodA{
   font-weight: bold;
   border-bottom: 1px solid black;
@@ -57,14 +79,63 @@ export default {
 }
 .header{
   width: 100%;
-  height: @height*3-1;
+  height: @headHeight;
+  max-height: @height*6;
   overflow: hidden;
+  display: flex;
+  align-items:center;
+  flex-warp:center;
+}
+.flexCenter{
   display: flex;
   align-items:center;
   flex-warp:center;
 }
 .header img{
   width: 100%;
+}
+.header .myInf{
+  width: @width;
+  height: @headHeight;
+  height: 100%;
+  background:rgba(255, 255, 255, 0.7);
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  box-sizing: border-box;
+  padding:@paddingTb;
+  .flexCenter
+}
+ .imgHead{
+  width: @list5;
+  height: @list5;
+  float: left;
+  .flexCenter;
+}
+.imgHead img{
+  width: 90%;
+  height: 90%;
+  border-radius: 50%;
+}
+.myInf p{
+  font-weight: bold;
+  text-align: left;
+  height: @list1;
+}
+.myInf p span{
+  font-size: @smallFont;
+  color: @lesstColor;
+  display: block;
+  line-height: @list2/1.3;
+}
+.myInf a{
+  font-size: @mainFont;
+  color: @infA;
+  margin:0 @list2/3 0 0;
+}
+.myInf a span{
+  font-weight: bold;
+  color: @mainColor;
 }
 .psCenter{
    position: absolute;
